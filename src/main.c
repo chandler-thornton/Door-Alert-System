0dd5a53c8748c7ab2e6fa6b34dc22799d9acea81
chandler.thornton <chanman1218@outlook.com>
2023-06-20 14:21:05 -0400

    first commit

---
+++ src/main.c
@@ -0,0 +1,336 @@
+/* GENERAL */
+#include <stdio.h>
+#include <string.h>
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "esp_crt_bundle.h"
+#include "esp_http_client.h"
+
+#define LOG_LEVEL_LOCAL     ESP_LOG_VERBOSE        
+#define LOG_TAG             "MAIN"
+
+/* GPIO */
+#define DOOR_SENSOR_PIN     GPIO_NUM_19
+#define LED_PIN             GPIO_NUM_13
+
+/* TWILIO */
+static const char *TAG = "HTTP_CLIENT";
+
+//Initialize info for message
+char account_sid[] = "AC9957b35538c35d162207516b86807962";
+char auth_token[] = "0332eacf0b9dd52973bd65303a3cfc96";
+char recipient_num[] = "+16784354993";
+char sender_num[] = "+18449924131";
+
+char message_open[] = "Bedroom: Door opened";
+char message_close[] = "Bedroom: Door closed";
+
+
+void twilio_send_sms_open(void *pvParameters)
+{
+
+    char twilio_url[200];
+    snprintf(twilio_url,
+             sizeof(twilio_url),
+             "%s%s%s",
+             "https://api.twilio.com/2010-04-01/Accounts/",
+             account_sid,
+             "/Messages");
+
+    char post_data[200];
+
+    snprintf(post_data,
+             sizeof(post_data),
+             "%s%s%s%s%s%s",
+             "To=",
+             recipient_num,
+             "&From=",
+             sender_num,
+             "&Body=",
+             message_open);
+
+    esp_http_client_config_t config = {
+        .url = twilio_url,
+        .method = HTTP_METHOD_POST,
+        .auth_type = HTTP_AUTH_TYPE_BASIC,
+        .transport_type = HTTP_TRANSPORT_OVER_SSL,  //Specify transport type
+        .crt_bundle_attach = esp_crt_bundle_attach, //Attach the certificate bundle 
+    };
+
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_http_client_set_header(client, "Content-Type", "application/x-www-form-urlencoded");
+    esp_http_client_set_username(client, account_sid);
+    esp_http_client_set_password(client, auth_token);
+
+    ESP_LOGI(TAG, "post = %s", post_data);
+    esp_http_client_set_post_field(client, post_data, strlen(post_data));
+
+    esp_err_t err = esp_http_client_perform(client);
+
+    if (err == ESP_OK)
+    {
+        int status_code = esp_http_client_get_status_code(client);
+        if (status_code == 201)
+        {
+            ESP_LOGI(TAG, "Message sent Successfully");
+        }
+        else
+        {
+            ESP_LOGI(TAG, "Message sent Failed");
+        }
+    }
+    else
+    {
+        ESP_LOGI(TAG, "Message sent Failed");
+    }
+    esp_http_client_cleanup(client);
+    vTaskDelete(NULL);
+}
+
+void twilio_send_sms_close(void *pvParameters)
+{
+
+    char twilio_url[200];
+    snprintf(twilio_url,
+             sizeof(twilio_url),
+             "%s%s%s",
+             "https://api.twilio.com/2010-04-01/Accounts/",
+             account_sid,
+             "/Messages");
+
+    char post_data[200];
+
+    snprintf(post_data,
+             sizeof(post_data),
+             "%s%s%s%s%s%s",
+             "To=",
+             recipient_num,
+             "&From=",
+             sender_num,
+             "&Body=",
+             message_close);
+
+    esp_http_client_config_t config = {
+        .url = twilio_url,
+        .method = HTTP_METHOD_POST,
+        .auth_type = HTTP_AUTH_TYPE_BASIC,
+        .transport_type = HTTP_TRANSPORT_OVER_SSL,  //Specify transport type
+        .crt_bundle_attach = esp_crt_bundle_attach, //Attach the certificate bundle 
+    };
+
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_http_client_set_header(client, "Content-Type", "application/x-www-form-urlencoded");
+    esp_http_client_set_username(client, account_sid);
+    esp_http_client_set_password(client, auth_token);
+
+    ESP_LOGI(TAG, "post = %s", post_data);
+    esp_http_client_set_post_field(client, post_data, strlen(post_data));
+
+    esp_err_t err = esp_http_client_perform(client);
+
+    if (err == ESP_OK)
+    {
+        int status_code = esp_http_client_get_status_code(client);
+        if (status_code == 201)
+        {
+            ESP_LOGI(TAG, "Message sent Successfully");
+        }
+        else
+        {
+            ESP_LOGI(TAG, "Message sent Failed");
+        }
+    }
+    else
+    {
+        ESP_LOGI(TAG, "Message sent Failed");
+    }
+    esp_http_client_cleanup(client);
+    vTaskDelete(NULL);
+}
+
+/* WIFI */
+
+#define EXAMPLE_ESP_WIFI_SSID      "Haven210"
+#define EXAMPLE_ESP_WIFI_PASS      "Mgirl210"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  10
+
+#if CONFIG_ESP_WPA3_SAE_PWE_HUNT_AND_PECK
+#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HUNT_AND_PECK
+#define EXAMPLE_H2E_IDENTIFIER ""
+#elif CONFIG_ESP_WPA3_SAE_PWE_HASH_TO_ELEMENT
+#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HASH_TO_ELEMENT
+#define EXAMPLE_H2E_IDENTIFIER CONFIG_ESP_WIFI_PW_ID
+#elif CONFIG_ESP_WPA3_SAE_PWE_BOTH
+#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_BOTH
+#define EXAMPLE_H2E_IDENTIFIER CONFIG_ESP_WIFI_PW_ID
+#endif
+#if CONFIG_ESP_WIFI_AUTH_OPEN
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_OPEN
+#elif CONFIG_ESP_WIFI_AUTH_WEP
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WEP
+#elif CONFIG_ESP_WIFI_AUTH_WPA_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA2_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA_WPA2_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_WPA2_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA3_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA3_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA2_WPA3_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_WPA3_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WAPI_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WAPI_PSK
+#endif
+
+// FreeRTOS event group to signal when we are connected
+static EventGroupHandle_t s_wifi_event_group;
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+static int s_retry_num = 0;
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            /* Authmode threshold resets to WPA2 as default if password matches WPA2 standards (pasword len => 8).
+             * If you want to connect the device to deprecated WEP/WPA networks, Please set the threshold value
+             * to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK and set the password with length and format matching to
+             * WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK standards.
+             */
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+}
+
+void app_main(void)
+{
+    /* INITIALIZE NVS */
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
+
+    /* INITIALIZE GPIOs */
+    int PREVIOUS_DOOR_PIN_LEVEL = gpio_get_level(DOOR_SENSOR_PIN);
+    int CURRENT_DOOR_PIN_LEVEL;
+
+    esp_rom_gpio_pad_select_gpio(DOOR_SENSOR_PIN);                   
+    gpio_set_direction(DOOR_SENSOR_PIN, GPIO_MODE_INPUT);        
+    gpio_set_pull_mode(DOOR_SENSOR_PIN, GPIO_PULLUP_ONLY);     
+
+    esp_rom_gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    /* MAIN LOOP */
+    while(true)    
+    {
+        CURRENT_DOOR_PIN_LEVEL = gpio_get_level(DOOR_SENSOR_PIN);
+        if(CURRENT_DOOR_PIN_LEVEL == 1 && PREVIOUS_DOOR_PIN_LEVEL == 0){
+            xTaskCreate(&twilio_send_sms_open, "twilio_send_sms_open", 8192, NULL, 6, NULL);
+        }    
+        else if(CURRENT_DOOR_PIN_LEVEL == 0 && PREVIOUS_DOOR_PIN_LEVEL == 1){
+            xTaskCreate(&twilio_send_sms_close, "twilio_send_sms_close", 8192, NULL, 6, NULL);
+        }
+        else if(CURRENT_DOOR_PIN_LEVEL == PREVIOUS_DOOR_PIN_LEVEL){
+            if(CURRENT_DOOR_PIN_LEVEL == 1){
+                gpio_set_level(LED_PIN, 1);
+            }
+            else{
+                gpio_set_level(LED_PIN, 0);
+            }
+        }
+        else{
+            ESP_LOGI(LOG_TAG, "ERROR");
+        }
+
+        PREVIOUS_DOOR_PIN_LEVEL = CURRENT_DOOR_PIN_LEVEL;
+    }
+}
